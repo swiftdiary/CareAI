0a088b175ae8366996ad5fc9847b0a4a92e03b97
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2024-02-17 11:34:24 +0500

    Initial Commit

---
+++ CareAI/CareAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareAIApp.swift
+//  CareAI
+//
+//  Created by Akbar Khusanbaev on 17/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CareAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
